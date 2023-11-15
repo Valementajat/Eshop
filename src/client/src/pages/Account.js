e2bd42207caeb2d07fdff3f0e6616f4ba537b34c
Joui569 <joelmiramolto@gmail.com>
2023-11-15 01:52:36 +0100

    Buttons

--- src/client/src/pages/Account.js
+++ src/client/src/pages/Account.js
@@ -3,10 +3,9 @@ import { Button, TextField, Alert } from "@mui/material";
 import { Link } from "react-router-dom";
 
 class Account extends Component {
-  constructor(props) {
+ constructor(props) {
     super(props);
     this.state = {
-
       message: null,
     };
     const localUser = JSON.parse(localStorage.getItem("user"));
@@ -14,26 +13,25 @@ class Account extends Component {
     if (!localUser || !localUser.token) {
       window.location.replace("/");
     } 
-  }
+ }
 
-  handleInputChange = (event) => {
+ handleInputChange = (event) => {
     this.setState({
       [event.target.name]: event.target.value,
     });
-  };
+ };
 
-  logout = () => {
+ logout = () => {
     // Remove the user from local storage
     localStorage.removeItem("user");
 
     // Optionally, redirect the user to the login page or another page
     window.location.replace("/login");
-  };
+ };
 
+ render() {
+    const user = JSON.parse(localStorage.getItem("user"));
 
-  render() {
-    let user = JSON.parse(localStorage.getItem("user"));
-    ;
     return (
       <div className="login-container">
         <h2>Account info</h2>
@@ -47,8 +45,7 @@ class Account extends Component {
               <span><strong>Email: </strong></span>
               <span>{user.email}</span>
             </div>
-            < hr />
-
+            <hr />
           </div>
         )}
         <Link to="/">
@@ -67,7 +64,20 @@ class Account extends Component {
         
       </div>
     );
-  }
+ }
+
+ render() {
+  return (
+     <div className="login-container">
+
+       <button onClick={this.handleButton1Click}>Increase</button>
+
+       <button onClick={this.handleButton2Click}>Decrease</button>
+
+       <div>{this.state.text}</div>
+     </div>
+  );
+ }
 }
 
 export default Account;
